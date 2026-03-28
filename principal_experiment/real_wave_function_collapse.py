dad8c67cbecc0df3d2d2de569276c25c70089ef1
Vitor985-hub <vitorfer2018@gmail.com>
2026-03-27 21:14:17 -0300

    add gitgnore

--- principal_experiment/real_wave_function_collapse.py
+++ principal_experiment/real_wave_function_collapse.py
@@ -1,4 +1,3 @@
-# real_wave_function_collapse.py
 from qiskit import QuantumCircuit, transpile
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from qiskit_ibm_runtime import QiskitRuntimeService, Sampler
